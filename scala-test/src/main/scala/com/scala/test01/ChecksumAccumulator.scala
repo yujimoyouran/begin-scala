25c9d983f8f455c456f9aac82d665880ea354c64
yujimoyouran <416387897@qq.com>
2016-05-02 21:15:45 +0800

    Companion Object

---
+++ scala-test/src/main/scala/com/scala/test01/ChecksumAccumulator.scala
@@ -0,0 +1,29 @@
+package com.scala.test01
+
+import scala.collection.mutable.Map
+
+class ChecksumAccumulator {
+  private var sum = 0
+  def add(b : Byte) = {sum += b}
+  def checksum() : Int = -(sum & 0xFF) + 1
+}
+
+object ChecksumAccumulator {
+  private val cache = Map[String, Int]()
+  
+  def calculate(s : String) : Int =
+    if(cache.contains(s))
+      cache(s)
+    else{
+      val acc = new ChecksumAccumulator
+      for(c <- s)
+        acc.add(c.toByte)
+      val cs = acc.checksum();
+      cache += (s -> cs)
+      cs
+    }
+  
+  def main(args: Array[String]): Unit = {
+    println(ChecksumAccumulator.calculate("this is first"))
+  }
+}
